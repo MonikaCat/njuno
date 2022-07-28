e0761c9394d4fe9d274c9b64d480f820735c67c3
Magic Cat <pusz.monika@gmail.com>
2022-07-28 13:51:01 +0100

    updated schema

--- database/schema/00-cosmos.sql
+++ database/schema/00-cosmos.sql
@@ -12,11 +12,11 @@ CREATE TABLE validator
 
 CREATE TABLE block
 (
-    height           BIGINT UNIQUE PRIMARY KEY,
-    hash             TEXT                        NOT NULL UNIQUE,
+    height           BIGINT  UNIQUE PRIMARY KEY,
+    hash             TEXT    NOT NULL UNIQUE,
     num_txs          INTEGER DEFAULT 0,
     total_gas        BIGINT  DEFAULT 0,
-    proposer_address TEXT REFERENCES validator (consensus_address),
+    proposer_address TEXT    REFERENCES validator (consensus_address),
     timestamp        TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 CREATE INDEX block_height_index ON block (height);
@@ -54,44 +54,3 @@ CREATE TABLE transaction
 CREATE INDEX transaction_hash_index ON transaction (hash);
 CREATE INDEX transaction_height_index ON transaction (height);
 CREATE INDEX transaction_partition_id_index ON transaction (partition_id);
-
-CREATE TABLE message
-(
-    transaction_hash            TEXT   NOT NULL,
-    index                       BIGINT NOT NULL,
-    type                        TEXT   NOT NULL,
-    value                       JSONB  NOT NULL,
-    involved_accounts_addresses TEXT[] NOT NULL,
-
-    /* PSQL partition */
-    partition_id                BIGINT NOT NULL DEFAULT 0,
-    height                      BIGINT NOT NULL,
-    FOREIGN KEY (transaction_hash, partition_id) REFERENCES transaction (hash, partition_id),
-    CONSTRAINT unique_message_per_tx UNIQUE (transaction_hash, index, partition_id)
-) PARTITION BY LIST (partition_id);
-CREATE INDEX message_transaction_hash_index ON message (transaction_hash);
-CREATE INDEX message_type_index ON message (type);
-CREATE INDEX message_involved_accounts_index ON message USING GIN(involved_accounts_addresses);
-
-/**
- * This function is used to find all the utils that involve any of the given addresses and have
- * type that is one of the specified types.
- */
-CREATE FUNCTION messages_by_address(
-    addresses TEXT[],
-    types TEXT[],
-    "limit" BIGINT = 100,
-    "offset" BIGINT = 0)
-    RETURNS SETOF message AS
-$$
-SELECT * FROM message
-WHERE (cardinality(types) = 0 OR type = ANY (types))
-  AND addresses && involved_accounts_addresses
-ORDER BY height DESC LIMIT "limit" OFFSET "offset"
-$$ LANGUAGE sql STABLE;
-
-CREATE TABLE pruning
-(
-    last_pruned_height BIGINT NOT NULL
-)
-
