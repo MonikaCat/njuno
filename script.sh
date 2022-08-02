eceb543c381d59f66874c8191ece9c8b079b4249
Magic Cat <pusz.monika@gmail.com>
2022-08-02 19:25:57 +0100

    add yamlfmt to  script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,10 @@
 echo '****** Updating Validators List ******'
 echo ''
 
-VALIDATORS_QUERY=$(nomic validators | tee . validators_list.yaml yamlfmt validators_list.yaml)
+QUERY_VALIDATORS_LIST=$(nomic validators | tee . validators_list.yaml)
+FORMAT_VALIDATORS_LIST=$(yamlfmt validators_list.yaml)
 
-echo $VALIDATOR_QUERY
+echo $QUERY_VALIDATORS_LIST
+echo $FORMAT_VALIDATORS_LIST
 echo ''
 echo '****** Saved updated list of validators! ******'
