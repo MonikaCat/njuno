cde66ebeee0ed402e36983d05cf3eef691d39a32
Magic Cat <pusz.monika@gmail.com>
2022-08-23 20:43:16 +0700

    add func to query latest validators

---
+++ modules/staking/utils/utils.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	types "github.com/MonikaCat/njuno/types"
+	"github.com/MonikaCat/njuno/types/config"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
+)
+
+// GetLatestValidatorsStatus queries the latest validators status and stores it inside yaml file,
+// returning an array of validators
+func GetLatestValidatorsStatus() *types.ValidatorsList {
+	validatorsCmd := exec.Command("sh", "-c", "/Users/monikapusz/Desktop/Github/njuno/query_validators_list.sh")
+
+	cmdOutput := &bytes.Buffer{}
+	validatorsCmd.Stdout = cmdOutput
+	validatorsCmd.Stderr = os.Stderr
+	if err := validatorsCmd.Run(); err != nil {
+		return nil
+	}
+
+	// Get the validators list
+	validatorsList := &types.ValidatorsList{}
+	yamlFile, err := ioutil.ReadFile(config.Cfg.Parser.ValidatorsListFilePath)
+	if err != nil {
+		log.Printf("error while reading validators list yaml file: %s ", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, validatorsList)
+	if err != nil {
+		log.Printf("error while unmarshaling validators list yaml file: %s ", err)
+	}
+
+	return validatorsList
+}
