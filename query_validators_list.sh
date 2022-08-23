be135fb11cf29cc3129cd415b326a4c042856a36
Magic Cat <pusz.monika@gmail.com>
2022-08-23 20:02:14 +0700

    updated script again

--- query_validators_list.sh
+++ query_validators_list.sh
@@ -6,9 +6,7 @@ echo ''
 QUERY_VALIDATORS_LIST=$(nomic validators 2>&1 | sed '1 i\
 validators: 
 ' |   tee ${HOME}/.njuno/validators_list.yaml)
-FORMAT_VALIDATORS_LIST=$(yamlfmt validators_list.yaml)
 
 echo $QUERY_VALIDATORS_LIST
-echo $FORMAT_VALIDATORS_LIST
 echo ''
 echo '****** Saved updated list of validators! ******'
