d09b08e420949ecad0fa8a5c41886594c8d404d1
Magic Cat <pusz.monika@gmail.com>
2022-08-24 14:33:53 +0700

    add ParseValidatorsList

--- modules/staking/utils/utils.go
+++ modules/staking/utils/utils.go
@@ -8,6 +8,7 @@ import (
 
 	types "github.com/MonikaCat/njuno/types"
 	"github.com/MonikaCat/njuno/types/config"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v2"
 )
@@ -37,4 +38,22 @@ func GetLatestValidatorsStatus() *types.ValidatorsList {
 	}
 
 	return validatorsList
+}
+
+func ParseValidatorsList(validatorsList *types.ValidatorsList) ([]types.Validator, []types.ValidatorDescription, []types.ValidatorCommission, []types.ValidatorStatus) {
+	var validators []types.Validator
+	var validatorsDescription []types.ValidatorDescription
+	var validatorsCommission []types.ValidatorCommission
+	var validatorsStatus []types.ValidatorStatus
+
+	for _, val := range validatorsList.Validators {
+		consAddr := sdk.ConsAddress(val.Validator.Address).String()
+
+		validators = append(validators, types.NewValidator(consAddr, val.Validator.Address, 1))
+		validatorsDescription = append(validatorsDescription, types.NewValidatorDescription(consAddr, val.Validator.Details, val.Validator.Identity, val.Validator.Moniker, 1))
+		validatorsCommission = append(validatorsCommission, types.NewValidatorCommission(consAddr, val.Validator.Commission, val.Validator.MinSelfDelegation, 1))
+		validatorsStatus = append(validatorsStatus, types.NewValidatorStatus(consAddr, val.Validator.InActiveSet, val.Validator.Jailed, val.Validator.Tombstoned, 1))
+	}
+
+	return validators, validatorsDescription, validatorsCommission, validatorsStatus
 }
