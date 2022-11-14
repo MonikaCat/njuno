b99c3882e1f23c767f72e8074267b1219a67b46a
Magic Cat <pusz.monika@gmail.com>
2022-11-14 13:31:47 +0700

    move update staking pool to periodic ops

--- modules/staking/handle_periodic_operations.go
+++ modules/staking/handle_periodic_operations.go
@@ -2,9 +2,12 @@ package staking
 
 import (
 	"fmt"
+	"strconv"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	staking "github.com/forbole/njuno/modules/staking/utils"
 	"github.com/forbole/njuno/modules/utils"
+	types "github.com/forbole/njuno/types"
 	"github.com/go-co-op/gocron"
 	"github.com/rs/zerolog/log"
 )
@@ -13,7 +16,7 @@ import (
 func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	log.Debug().Str("module", "staking").Msg("setting up periodic tasks")
 
-	// Fetch updated validators info every 5 mins
+	// Fetch updated validators and staking pool info every 5 mins
 	if _, err := scheduler.Every(5).Minutes().Do(func() {
 		utils.WatchMethod(m.updateValidatorsInfo)
 	}); err != nil {
@@ -23,7 +26,8 @@ func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
 	return nil
 }
 
-// updateValidatorsInfo allows to get the latest validators infos from yaml file
+// updateValidatorsInfo allows to parse the latest validators infos from yaml file,
+// update staking pool and store it inside the database
 func (m *Module) updateValidatorsInfo() error {
 	log.Debug().
 		Str("module", "staking").
@@ -34,6 +38,7 @@ func (m *Module) updateValidatorsInfo() error {
 	if err != nil {
 		return fmt.Errorf("error while getting latest block height, error: %s", err)
 	}
+
 	// query the latest validators list
 	validatorsLists := staking.GetLatestValidatorsList()
 
@@ -70,5 +75,39 @@ func (m *Module) updateValidatorsInfo() error {
 			Msg("error while saving validators voting power")
 	}
 
+	// update staking pool after updating validators VP
+	// as bonded tokens correspond to overall voting power
+	go m.updateStakingPool(height, validatorsVP)
+
 	return nil
+}
+
+// updateStakingPool reads the current staking pool and stores its value inside the database
+func (m *Module) updateStakingPool(height int64, validatorsVP []types.ValidatorVotingPower) {
+	log.Debug().Str("module", "staking").Int64("height", height).
+		Msg("updating staking pool")
+
+	// Hard code total supply of 21M NOM
+	var totalTokenSupply int64 = 21000000000000
+	var bondedTokens int64
+
+	// Calculate the overall voting power as total bonded tokens value
+	for _, vp := range validatorsVP {
+		v, err := strconv.ParseInt(vp.VotingPower, 10, 64)
+		if err != nil {
+			fmt.Errorf("failed to parse voting power from string to int: %s", err)
+		}
+		bondedTokens += v
+	}
+
+	// Calculate not bonded tokens by substracting bonded tokens
+	// from the total token supply value
+	var notBondedTokens = totalTokenSupply - bondedTokens
+
+	err := m.db.SaveStakingPool(types.NewStakingPool(sdk.NewInt(bondedTokens), sdk.NewInt(notBondedTokens), height))
+	if err != nil {
+		log.Error().Str("module", "staking").Err(err).Int64("height", height).
+			Msg("error while saving staking pool")
+		return
+	}
 }
